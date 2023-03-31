a780716fd48fe8606b24a676fd8fd96568339342
Valery Akhrymchuk <valeryakhrymchuk@gmail.com>
2023-03-31 23:54:46 +0300

    New sensors position and calibration

--- src/hal/Sensors.h
+++ src/hal/Sensors.h
@@ -14,15 +14,15 @@ class Sensors {
 public:
 
     TimingSensor *left0 = createSensor(
-            new Sharp10_150Table(A0, new uint16_t[READING_COUNT]{843, 593, 422, 307, 238, 190, 159, 152, 148}));
+            new Sharp10_150Table(A0, new uint16_t[READING_COUNT]{625, 387, 295, 214, 167, 145, 132, 115, 114}));
     TimingSensor *right0 = createSensor(
-            new Sharp10_150Table(A1, new uint16_t[READING_COUNT]{820, 583, 422, 306, 234, 182, 153, 146, 140}));
+            new Sharp10_150Table(A1, new uint16_t[READING_COUNT]{612, 359, 276, 194, 145, 121, 97, 80, 75}));
     TimingSensor *right45 = createSensor(
-            new Sharp10_150Table(A6, new uint16_t[READING_COUNT]{871, 620, 419, 293, 214, 159, 137, 132, 123}));
+            new Sharp10_150Table(A6, new uint16_t[READING_COUNT]{611, 350, 255, 177, 130, 103, 78, 60, 49}));
     TimingSensor *left45 = createSensor(
-            new Sharp10_150Table(A7, new uint16_t[READING_COUNT]{876, 594, 428, 306, 231, 179, 146, 142, 135}));
+            new Sharp10_150Table(A7, new uint16_t[READING_COUNT]{620, 360, 268, 183, 133, 108, 81, 62, 51}));
     TimingSensor *forward = createSensor(
-            new Sharp100_500Table(A2, new uint16_t[READING_COUNT]{627, 556, 502, 369, 319, 300, 287, 277, 270}));
+            new Sharp100_500Table(A2, new uint16_t[READING_COUNT]{627, 556, 507, 379, 327, 302, 287, 277, 270}));
 
     int forwardD = 0;
     int r0d = 0;
@@ -42,12 +42,12 @@ public:
             Serial.print(millis());
             Serial.print(" left45 = ");
             Serial.print(l45d);
-            Serial.print(" right0 = ");
-            Serial.print(r0d);
-            Serial.print(" forward = ");
-            Serial.print(forwardD);
             Serial.print(" left0 = ");
             Serial.print(l0d);
+            Serial.print(" forward = ");
+            Serial.print(forwardD);
+            Serial.print(" right0 = ");
+            Serial.print(r0d);
             Serial.print(" right45 = ");
             Serial.print(r45d);
             Serial.println();
@@ -77,8 +77,8 @@ private:
     Interval debug = Interval(200);
 
     static TimingSensor *createSensor(DistanceSensor *sensor) {
-        return new TimingSensor((sensor));
-//        return new TimingSensor(new MedianFilterWindow(sensor));
+//        return new TimingSensor((sensor));
+        return new TimingSensor(new MedianFilterWindow(sensor));
     }
 
 };
