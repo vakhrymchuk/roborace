9c0fb425ffcb836c3f50ed925effda1c69595d54
Valery Akhrymchuk <valeryakhrymchuk@gmail.com>
2026-06-10 14:51:32 +0300

    tune 5

--- src/Roborace2.h
+++ src/Roborace2.h
@@ -78,7 +78,7 @@ public:
 
         // Уменьшаем скорость при yaw 160-200 градусов (в диапазоне -180..180 это 160..180 и -180..-160)
         if (mpu.yaw >= 155 || mpu.yaw < -160) {
-            speed = min(speed, 50);
+            speed = min(speed, 55);
         }
 
         pid.setpoint(desiredAngle);
--- src/hal/LineSensor.h
+++ src/hal/LineSensor.h
@@ -11,7 +11,7 @@ enum class LineSensorState {
 
 class LineSensor {
 public:
-    static constexpr unsigned long BLACK_LINE_MIN_MS = 40;  // ~5 см при 150 см/с
+    static constexpr unsigned long BLACK_LINE_MIN_MS = 35;  // ~5 см при 150 см/с
     static constexpr unsigned long STOP_DURATION_MS = 1000;
     static constexpr unsigned long IGNORE_AFTER_STOP_MS = 5000;
 
